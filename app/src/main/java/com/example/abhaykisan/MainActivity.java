b6d6dbbffac8b02026f9451abe906eb91a2f23d6
VedantSonje <vedant.sonje12@gmail.com>
2020-12-10 18:36:27 +0530

    Camera

--- app/src/main/java/com/example/abhaykisan/MainActivity.java
+++ app/src/main/java/com/example/abhaykisan/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.abhaykisan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnDiseasesIdentification;
+    Button btnWeedDetection;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnDiseasesIdentification =  findViewById(R.id.btnDiseasesIdentification);
+
+        btnWeedDetection = findViewById(R.id.btnWeedDetection);
+
+        btnDiseasesIdentification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(MainActivity.this,TakeSnap.class);
+                startActivity(i);
+
+            }
+        });
+
+        btnWeedDetection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(MainActivity.this,TakeSnap.class);
+                startActivity(i);
+
+            }
+        });
+
     }
 }
