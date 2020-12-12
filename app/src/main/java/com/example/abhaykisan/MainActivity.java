7c746663255483fb0d5e076bfa59b4ce9f4a02b3
Aakash_Barhate <2017.aakash.barhate@ves.ac.in>
2020-12-12 12:57:18 +0530

    main activity ui improvement

--- app/src/main/java/com/example/abhaykisan/MainActivity.java
+++ app/src/main/java/com/example/abhaykisan/MainActivity.java
@@ -6,17 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btnCropRecommender;
 
     Button btnDiseasesIdentification;
     Button btnWeedDetection;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnDiseasesIdentification =  findViewById(R.id.btnDiseasesIdentification);
+        btnDiseasesIdentification = findViewById(R.id.btnDiseasesIdentification);
         btnCropRecommender = findViewById(R.id.btnCropRecommender);
         btnWeedDetection = findViewById(R.id.btnWeedDetection);
 
@@ -24,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent i = new Intent(MainActivity.this,TakeSnap.class);
+                Intent i = new Intent(MainActivity.this, TakeSnap.class);
                 startActivity(i);
 
             }
@@ -34,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent i = new Intent(MainActivity.this,CropRecommendation.class);
+                Intent i = new Intent(MainActivity.this, CropRecommendation.class);
                 startActivity(i);
 
             }
@@ -44,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent i = new Intent(MainActivity.this,TakeSnap.class);
+                Intent i = new Intent(MainActivity.this, TakeSnap.class);
                 startActivity(i);
 
             }
